ed740537b11b13e1b9165080a17b4123e6fb144d
Rviro <punisher2291@mail.ru>
2025-02-19 16:30:49 +0300

    english better

--- string3.sh
+++ string3.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# Ввод данных от пользователя
+# input string
 read -p "Введите строку: " input_string
 read -p "Введите начало диапазона: " start
 read -p "Введите конец диапазона: " end
 
-# Диалог для выбора действия
+# choice
 echo "Выберите действие:"
 echo "1. Вывести подстроку"
 echo "2. Удалить подстроку"
 read -p "Ваш выбор (1 или 2): " choice
 
-# Корректируем индексы, так как в Bash индексация начинается с 1
+# index+1
 start=$((start - 1))
 end=$((end - 1))
 
 if [ "$choice" -eq 1 ]; then
-    # Выводим подстроку от начала до конца
+    # show string
     substring=$(echo "$input_string" | cut -c $((start + 1))-$((end + 1)))
     echo "Результат: $substring"
 elif [ "$choice" -eq 2 ]; then
-    # Удаляем подстроку и возвращаем оставшуюся часть строки
+    # delete string and show
     result="${input_string:0:start}${input_string:end+1}"
     echo "Результат: $result"
 else
